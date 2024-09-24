2e0ababd6196721ffd08f7f950ddfc3f4ee6c848
Michael Dennis <mdennis281@gmail.com>
2024-09-24 14:28:00 -0500

    revive tests

--- src/lanscape/tests/test_api.py
+++ src/lanscape/tests/test_api.py
@@ -1,14 +1,14 @@
-import _to_parent
+#import _to_parent
 import unittest
 import json
-from app import app
-from libraries.net_tools import get_network_subnet
-from libraries.subnet_scan import cleanup_old_jobs
+from ..app import app
+from ..libraries.net_tools import get_network_subnet
+from ..libraries.subnet_scan import cleanup_old_jobs
+from ._helpers import right_size_subnet
 
 
 
-
-class AppTestCase(unittest.TestCase):
+class ApiTestCase(unittest.TestCase):
     app = app.test_client()
 
     def test_port_lifecycle(self):
@@ -65,24 +65,75 @@ class AppTestCase(unittest.TestCase):
         response = self.app.post('/api/port/list/test_port_list_scan', json=new_port_list)
         self.assertEqual(response.status_code, 200)
 
-        # Create a new scan
-        new_scan = {'subnet': get_network_subnet(), 'port_list': 'test_port_list_scan'}
+        # Create a new scan, wait for completion
+        new_scan = {'subnet': right_size_subnet(get_network_subnet()), 'port_list': 'test_port_list_scan'}
         response = self.app.post('/api/scan/async', json=new_scan)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.data)['status'], 'complete')
+        scan_info = json.loads(response.data)
+        self.assertEqual(scan_info['status'], 'complete')
+
+        # Validate the scan worked without error
+        response = self.app.get(f"/api/scan/{scan_info['scan_id']}")
+        self.assertEqual(response.status_code, 200)
+        scan_data = json.loads(response.data)
+        self.assertEqual(scan_data['errors'], [])
+        self.assertEqual(scan_data['stage'],'complete')
+
 
         # Delete the new port list
         response = self.app.delete('/api/port/list/test_port_list_scan')
         self.assertEqual(response.status_code, 200)
 
-        cleanup_old_jobs(True)
+        cleanup_old_jobs()
+    
+    def test_subnet_ports(self):
+        """
+        Test to ensure multi-subnet dectection is working
+        """
+
+        response = self.app.get('/api/tools/subnet/list')
+        self.assertEqual(response.status_code, 200)
+
+        subnets = json.loads(response.data)
+        self.assertIsNot(len(subnets), 0)
+        self.assertIsInstance(subnets[0],dict)
+        subnet:dict = subnets[0]
+        self.assertIsNotNone(subnet.get('address_cnt'))
+
+    def test_subnet_validation(self):
+        """
+        test subnet validation and parsing is working as expected
+        """
+        subnet_tests = {
+            # subnet : count (-1 == invalid)
+            '10.0.0.0/24': 254,
+            '10.0.0.2/24': 254,
+            '10.0.0.1-100': 100,
+            '192.168.1.1/25': 126,
+            '10.0.0.1/24, 192.168.1.1-100': 354,
+            '10.0.0.1/20': 4094,
+            '10.0.0.1/19': 8190,
+            '': -1, # blank
+            '10.0.1/24': -1, # invalid
+            '10.0.0.1/2': -1, # too big
+            '10.0.0.1/19, 192.168.1.1/20': -1 # too big
+        }
+
+        for subnet, count in subnet_tests.items():
+            uri = f'/api/tools/subnet/test?subnet={subnet}'
+            response = self.app.get(uri)
+            self.assertEqual(response.status_code, 200)
+
+            data:dict = json.loads(response.data)
+            self.assertEqual(data.get('count'),count)
+            self.assertIsNotNone(data.get('msg'))
+            if count == -1:
+                self.assertFalse(data.get('valid'))
+            
+
+
+
 
         
-        
-
-
-
-
-
 if __name__ == '__main__':
     unittest.main()

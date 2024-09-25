3269eb16af5c0c69b2ad4cce6813cad4a2da06a9
Michael Dennis <mdennis281@gmail.com>
2024-09-25 09:19:44 -0500

    add a shutdown endpoint

--- src/lanscape/app.py
+++ src/lanscape/app.py
@@ -2,8 +2,9 @@ from flask import Flask
 import logging
 import multiprocessing
 import threading
-
+import os
 app = Flask(__name__)
+log = logging.getLogger('core')
 
 # Import Blueprints
 from .blueprints.api import api_bp
@@ -13,12 +14,27 @@ from .blueprints.web import web_bp
 app.register_blueprint(api_bp)
 app.register_blueprint(web_bp)
 
+
     
 # Custom Jinja filter
 def is_substring_in_values(results: dict, substring: str) -> bool:
     return any(substring.lower() in str(v).lower() for v in results.values()) if substring else True
 
 app.jinja_env.filters['is_substring_in_values'] = is_substring_in_values
+
+# External hook to kill flask server
+exiting = False
+@app.route("/shutdown")
+def exit_app():
+    global exiting
+    exiting = True
+    log.info('Received external exit request. Terminating flask.')
+    return "Done"
+
+@app.teardown_request
+def teardown(exception):
+    if exiting:
+        os._exit(0)
 
 
 ## Webserver creation functions
@@ -37,10 +53,14 @@ def start_webserver(debug: bool=True, port: int=5001) -> int:
         disable_flask_logging()
     app.run(host='0.0.0.0', port=port, debug=debug)
 
+
 def disable_flask_logging() -> None:
-    log = logging.getLogger('werkzeug')
-    log.setLevel(logging.ERROR)
+    flask_log = logging.getLogger('werkzeug')
+    flask_log.setLevel(logging.ERROR)
     #app.logger.disabled = True
+
 
 if __name__ == "__main__":
     start_webserver(True)
+
+

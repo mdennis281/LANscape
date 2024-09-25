d0bbd2fcc608e24e6b2655e7d02c4bb96226f80b
Michael Dennis <mdennis281@gmail.com>
2024-09-25 09:24:22 -0500

    fix bug where error thrown on invalid interface (filter invalid better)

--- src/lanscape/libraries/errors.py
+++ src/lanscape/libraries/errors.py
@@ -4,5 +4,4 @@ class SubnetTooLargeError(Exception):
     """Custom exception raised when the subnet size exceeds the allowed limit."""
     def __init__(self, subnet):
         self.subnet = subnet
-        self.max_ips = max_ips
         super().__init__(f"Subnet {subnet} exceeds the limit of IP addresses.")

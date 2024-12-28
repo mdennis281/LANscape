c5a47965603fb8b13c8b070244a9d3b3a900ea4d
Michael Dennis <mdennis281@gmail.com>
2024-12-27 19:12:42 -0600

    1.2.6 - Json dependency fix (#12)
    fixed an issue coming from 1.2.5 that caused the port and maclookup json files to disappear from published package.
    
    added enhancements to package script

--- package.ps1
+++ package.ps1
@@ -1,20 +1,38 @@
-# Run Python unit tests
-$unittestResult = python -m unittest
+# Usage:
+# .\package.ps1 
+# .\package.ps1 -SkipTests
+# .\package.ps1  -SkipUpload
+# .\package.ps1  -SkipUpload -SkipTests
 
-# Check if the tests succeeded
-if ($LASTEXITCODE -eq 0) {
-    # Remove files in dist directory
-    Remove-Item -Path .\dist\* -Recurse -Force
+param (
+    [switch]$SkipTests,
+    [switch]$SkipUpload
+)
 
-    # Upgrade pip and build the package
-    py -m pip install --upgrade pip
-    py -m pip install --upgrade build
-    py -m build
+if (-not $SkipTests) {
+    # Run Python unit tests
+    python -m unittest
 
+    # Check if the tests succeeded
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Unit tests failed. Exiting script." -ForegroundColor Red
+        exit $LASTEXITCODE
+    }
+}
+
+# Remove files in dist directory
+Remove-Item -Path dist -Recurse -Force
+Remove-Item -Path ./**/*.egg-info -Recurse -Force
+
+# Upgrade pip and build the package
+py -m pip install --upgrade pip
+py -m pip install --upgrade build
+py -m build
+
+if (-not $SkipUpload) {
     # Upgrade twine and upload the package
     py -m pip install --upgrade twine
+
+    py -m twine check dist/*
     py -m twine upload --repository pypi dist/*
-} else {
-    Write-Host "Unit tests failed. Exiting script." -ForegroundColor Red
-    exit $LASTEXITCODE
 }

2126d893ae123db6c5c6f3b4813f4323244f8a5a
Michael Dennis <mdennis281@gmail.com>
2025-05-27 05:09:46 -0500

    improved error handling for routes with missing scans

--- src/lanscape/ui/blueprints/web/routes.py
+++ src/lanscape/ui/blueprints/web/routes.py
@@ -18,45 +18,52 @@ def index():
     port_list = 'medium'
     parallelism = 0.7
     if scan_id := request.args.get('scan_id'): 
-        if scanner := scan_manager.get_scan(scan_id):
-            scan = scanner.results.export()
-            subnet = scan['subnet']
-            port_list = scan['port_list']
-            parallelism = scan['cfg']['t_multiplier']
-        else:
-            log.debug(f'Redirecting, scan {scan_id} doesnt exist in memory')
-            return redirect('/')
+        if scan := scan_manager.get_scan(scan_id):
+            subnet = scan.cfg.subnet
+            port_list = scan.cfg.port_list
+            parallelism = scan.cfg.t_multiplier
+            
+
+        log.debug(f'Redirecting, scan {scan_id} doesnt exist in memory')
+        return redirect('/')
     return render_template(
-        'main.html',
-        subnet=subnet, 
-        port_list=port_list, 
-        parallelism=parallelism,
-        alternate_subnets=subnets
-    )
+            'main.html',
+            subnet=subnet, 
+            port_list=port_list, 
+            parallelism=parallelism,
+            alternate_subnets=subnets
+        )
+    
 
 @web_bp.route('/scan/<scan_id>', methods=['GET'])
 @web_bp.route('/scan/<scan_id>/<section>', methods=['GET'])
 def render_scan(scan_id, section='all'):
-    scanner = scan_manager.get_scan(scan_id)
-    data = scanner.results.export()
-    filter = request.args.get('filter')
-    return render_template('scan.html', data=data, section=section, filter=filter)
+    if scanner := scan_manager.get_scan(scan_id):
+        data = scanner.results.export()
+        filter = request.args.get('filter')
+        return render_template('scan.html', data=data, section=section, filter=filter)
+    log.debug(f'Redirecting, scan {scan_id} doesnt exist in memory')
+    return redirect('/')
 
 @web_bp.route('/errors/<scan_id>')
 def view_errors(scan_id):
-    scanner = scan_manager.get_scan(scan_id)
-    data = scanner.results.export()
-    return render_template('scan/scan-error.html',data=data)
+    if scanner := scan_manager.get_scan(scan_id):
+        data = scanner.results.export()
+        return render_template('scan/scan-error.html',data=data)
+    log.debug(f'Redirecting, scan {scan_id} doesnt exist in memory')
+    return redirect('/')
 
 @web_bp.route('/export/<scan_id>')
 def export_scan(scan_id):
-    scanner = scan_manager.get_scan(scan_id)
-    export_json = scanner.results.export(str)
-    return render_template(
-        'scan/export.html',
-        scan=scanner,
-        export_json=export_json
-    )
+    if scanner := scan_manager.get_scan(scan_id):
+        export_json = scanner.results.export(str)
+        return render_template(
+            'scan/export.html',
+            scan=scanner,
+            export_json=export_json
+        )
+    log.debug(f'Redirecting, scan {scan_id} doesnt exist in memory')
+    return redirect('/')
 
 @web_bp.route('/shutdown-ui')
 def shutdown_ui():

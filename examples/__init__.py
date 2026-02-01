2782ce65ed3c4075a6baf2c67bf0cc6dfa587565
Michael Dennis <mdennis281@gmail.com>
2026-01-31 22:44:13 -0600

    resolved linters

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,4 +1,7 @@
-import lanscape
-import sys
+"""Example scripts demonstrating LANscape usage."""
 import os
+import sys
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import lanscape  # noqa: F401,E402  # pylint: disable=wrong-import-position,unused-import
